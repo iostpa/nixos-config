ab3a4e5869165f0c5e6f0e6dc78e9179fc5edadd
CuteDog5695 <cutedog5695@gmail.com>
2024-10-07 17:40:19 +0300

    Small but big typo

--- configuration.nix
+++ configuration.nix
@@ -116,7 +116,7 @@ in
       dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
       localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
 };
-    starship.enable = true
+    starship.enable = true;
   };
 
 
