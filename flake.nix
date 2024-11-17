6e22f8e7605e992e8e1101e2e3941590f570db54
orangci <orangc@proton.me>
2024-11-17 17:51:42 +0300

    style(alejandra): format all

--- flake.nix
+++ flake.nix
@@ -3,8 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     nix-gaming.url = "github:fufexan/nix-gaming";
 
@@ -14,20 +16,19 @@
     };
   };
 
-  outputs = { nixpkgs, ... } @ inputs: 
-    let
-      system = "x86_64-linux";
-    in {
-      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-        system = system;
-        specialArgs = { 
-          inherit inputs;
-          inherit system;
-        };
-        modules = [
+  outputs = {nixpkgs, ...} @ inputs: let
+    system = "x86_64-linux";
+  in {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = system;
+      specialArgs = {
+        inherit inputs;
+        inherit system;
+      };
+      modules = [
         ./configuration.nix
         {nixpkgs.overlays = [inputs.hyprpanel.overlay];}
-        ];
+      ];
     };
   };
 }
