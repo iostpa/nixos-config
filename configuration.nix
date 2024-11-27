d4a0835e66cc803f3524f061a903f00b25f10c64
CuteDog5695 <cutedog5695@gmail.com>
2024-11-27 22:31:20 +0200

    Add via flake with a new and active repo

--- configuration.nix
+++ configuration.nix
@@ -231,6 +231,7 @@
     pkgs.lazygit
     pkgs.microfetch
     pkgs.kdePackages.filelight
+    inputs.zen-browser.packages."${pkgs.system}".default
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -258,5 +259,5 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+  system.stateVersion = "25.05"; # Did you read the comment?
 }
